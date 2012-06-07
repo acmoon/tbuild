4d96e267606bedcf2430a1f7f8f2a01f29af31d6
daxingplay <daxingplay@gmail.com>
2012-06-07 22:07:12 +0800

    multi base, change config type

--- test/complierTest.js
+++ test/complierTest.js
@@ -1,13 +1,44 @@
 ﻿var ModuleComplier = require('../lib/base').ModuleComplier;
 
-ModuleComplier.build({
-    target:'F:/testbuild/assets/v2/sh/combo/detail.js',
-    base: [
-        'F:\\testbuild\\assets\\v2',
-        'F:\\SVN\\fed\\kissy-team\\kissy\\src'
+ModuleComplier.config({
+    packages: [
+        {
+            name: 'sh',
+            path: 'F:\\testbuild\\assets\\v2\\',
+            charset: 'gbk'
+        }
     ],
-    debug: true,
-    inputEncoding:'gbk',
-    outputEncoding:'gbk',
-    output:'f:\\testbuild'
+    suffix: ''
 });
+
+//ModuleComplier.build('F:/testbuild/assets/v2/sh/combo/detail.js', 'F:/testbuild/v2/sh/combo/detail.js');
+
+ModuleComplier.config({
+    packages: [{
+        name: 'core',
+        path: 'F:/Website/htdocs/secondhand/assets/user-credits/1.0/',
+        charset: 'gbk'
+    },{
+        name: 'mods',
+        path: 'F:/Website/htdocs/secondhand/assets/user-credits/1.0/',
+        charset: 'gbk'
+    },{
+        name: 'utils',
+        path: 'F:/Website/htdocs/secondhand/assets/',
+        charset: 'gbk'
+    }]
+});
+
+ModuleComplier.build('F:/Website/htdocs/secondhand/assets/user-credits/1.0/core/user-credits.js', 'F:/testbuild/v2/sh/combo/user-credit.js');
+
+//ModuleComplier.build({
+//    target:'F:/testbuild/assets/v2/sh/combo/detail.js',
+//    base: [
+//        'F:\\testbuild\\assets\\v2',
+//        'F:\\SVN\\fed\\kissy-team\\kissy\\src'
+//    ],
+//    debug: true,
+//    inputEncoding: 'gbk',
+//    outputEncoding:'gbk',
+//    output:'f:\\testbuild'
+//});
